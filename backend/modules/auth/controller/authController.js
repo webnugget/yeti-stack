fd5260d03ad262cfdd326697ca4f5203eb3291f8
webnugget <info@webnugget.de>
2015-03-14 15:03:19 +0100

    added auth via mongoDB

--- backend/modules/auth/controller/authController.js
+++ backend/modules/auth/controller/authController.js
@@ -1,17 +1,7 @@
 'use strict';
 var _ = rq('lodash'),
-    jwt = rq('jsonwebtoken');
-var users = [{
-    id: 1,
-    username: 'admin',
-    password: 'admin',
-    roles: ['admin', 'registered']
-}, {
-    id: 2,
-    username: 'user',
-    password: 'user',
-    roles: ['registered']
-}];
+    jwt = rq('jsonwebtoken'),
+    User = rq('userModel');
 
 function createToken(user) {
     return jwt.sign(_.omit(user, 'password'), process.env.SECRET || 'fapp-stack-secret', {
@@ -23,45 +13,62 @@ module.exports.login = function(req, res) {
         return res.status(400)
             .send("You must send the username and the password");
     }
-    var user = _.find(users, {
-        username: req.body.username
+    process.nextTick(function() {
+        User.findOne({
+            username: req.body.username
+        })
+            .select('+password')
+            .exec(function(err, user) {
+                if (err || !user) {
+                    return res.status(401)
+                        .send("The username or password don't match");
+                }
+                user.comparePassword(req.body.password, function(err, isMatch) {
+                    if (isMatch && !err) {
+                        res.status(201)
+                            .send({
+                                token: createToken(user)
+                            });
+                    } else {
+                        return res.status(401)
+                            .send("The username or password don't match");
+                    }
+                });
+            });
     });
-    if (!user) {
-        return res.status(401)
-            .send("The username or password don't match");
-    }
-    if (user.password !== req.body.password) {
-        return res.status(401)
-            .send("The username or password don't match");
-    }
-    res.status(201)
-        .send({
-            token: createToken(user)
-        });
 };
 module.exports.signUp = function(req, res) {
-    if (!req.body.username || !req.body.password) {
+    delete req.body.roles;
+    if (!req.body.username || !req.body.password || !req.body.email) {
         return res.status(400)
-            .send("You must send the username and the password");
+            .send("You must send the username, password and email");
     }
-    if (_.find(users, {
+    User.find({
         username: req.body.username
-    })) {
-        return res.status(400)
-            .send("A user with that username already exists");
-    }
-    var profile = _.pick(req.body, 'username', 'password', 'extra');
-    profile.id = _.max(users, 'id')
-        .id + 1;
-    profile.roles = ['registered'];
-    users.push(profile);
-    res.status(201)
-        .send({
-            token: createToken(profile)
+    })
+        .exec(function(err, users) {
+            if (users.length) {
+                return res.status(400)
+                    .send("A user with that username already exists");
+            } else {
+                var user = new User();
+                user = _.extend(user, req.body);
+                user.save(function(err) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(500)
+                            .send("Something went wrong while creating your account");
+                    } else {
+                        return res.status(201)
+                            .send({
+                                token: createToken(user)
+                            });
+                    }
+                });
+            }
         });
 };
 module.exports.newToken = function(req, res) {
-    console.log(req.user);
     res.status(201)
         .send({
             token: createToken(req.user)
